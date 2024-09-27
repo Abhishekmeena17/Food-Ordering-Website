70cbe424f239c4cd373a0316beec8d57c38d530e
Abhishek Meena <abhishekmeena405@gmail.com>
2024-09-27 20:59:46 +0530

    updatew

--- frontend/src/components/Meals.jsx
+++ frontend/src/components/Meals.jsx
@@ -6,7 +6,7 @@ export default function Meals() {
 
   useEffect(() => {
     async function fetchMeals() {
-      const response = await fetch('http://localhost:3000/meals');
+      const response = await fetch('https://food-ordering-website-1-rvo1.onrender.com/meals');
 
       if (!response.ok) {
         //..
